0970b056e6778a7ad063a9af21f754a13d91ecf5
Johnny A <39755359+ITGoon@users.noreply.github.com>
2019-06-24 22:04:12 -0700

    Initial

---
+++ submit.php
@@ -0,0 +1,25 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "jsajsa", "captive_data");
+
+// Check connection
+//if($link === false){
+//    die("ERROR: Could not connect. " . mysqli_connect_error());
+//}
+
+// Escape user inputs for security
+$emails = mysqli_real_escape_string($link, $_REQUEST['emails']);
+
+// attempt insert query execution
+$sql = "INSERT INTO users (emails) VALUES ('$emails')";
+//if(mysqli_query($link, $sql)){
+ //   echo "Records added successfully.";
+//} else{
+//    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// close connection
+mysqli_close($link);
+header( 'Location: success.html' ) ;
+?>
