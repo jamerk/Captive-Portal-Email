6ebd0e3e08afc65548c2daba313bdf130acc899e
Johnny A <39755359+ITGoon@users.noreply.github.com>
2019-12-26 18:17:58 -0800

    Update submit.php

--- ruckus/submit.php
+++ ruckus/submit.php
@@ -1,6 +1,4 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "captive", "YourCaptiveUserPassword", "captive_data");
 
 // Check connection
